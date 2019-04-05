96224cca7cbae29582e12af49472020b7d9ba7f9
shaik Khaja Hussain <hussainshaik18@gmail.com>
2019-04-05 10:16:04 +0530

    DataTypes-Strings

---
+++ JavaScript_Fundamentals/DataTypes/dataTypes_strings.js
@@ -0,0 +1,12 @@
+// remember that a string should always be enclosed within quotes - single '' or double "".
+//Q1
+// Variable to define Sixth planet of our Solar System in a variable by name planet
+var planet ="Jupiter";
+// Print the variable here
+console.log(planet);
+
+
+/** ESCAPE NOTATION  */
+
+//C:\Program Files\nodejs
+console.log("C:\\Program Files\\nodejs");
